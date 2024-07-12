ade03f8b6ff2072bbae87860a95d5a6e40cd148c
coreyeross25 <coreyeross12@gmail.com>
2024-07-12 04:30:37 +0000

    practice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Naive Ticket running.");
   }
-
+// coreye
   // @Test
   // void addition() {
   //     assertEquals(2, 1 + 1);
